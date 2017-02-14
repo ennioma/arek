0e2d82c9c01cba258b56cdf48e735e01efc706f0
Augusto Souza <augustorsouza@gmail.com>
2017-02-14 10:19:25 -0200

    Allow configuration to change in ArekNotifications

--- arek/Classes/Permissions/ArekNotifications.swift
+++ arek/Classes/Permissions/ArekNotifications.swift
@@ -14,10 +14,11 @@ import UserNotifications
 open class ArekNotifications: ArekBasePermission, ArekPermissionProtocol {
     open var identifier: String = "ArekNotifications"
     
-    public init() {
+    public init(configuration: ArekConfiguration? = nil) {
         let data = ArekLocalizationManager(permission: self.identifier)
         
-        super.init(initialPopupData: ArekPopupData(title: data.initialTitle, message: data.initialMessage, image: data.image),
+        super.init(configuration: configuration,
+                   initialPopupData: ArekPopupData(title: data.initialTitle, message: data.initialMessage, image: data.image),
                    reEnablePopupData: ArekPopupData(title: data.reEnableTitle, message:  data.reEnableMessage, image: data.image))
     }
 
