19b4142929a7826426ed4efe059c76122bb2de0c
EnnioMa <ennio.masi@gmail.com>
2017-08-26 14:34:41 +0100

    General fixes (#88)
    * README updated with CI badges
    
    * LICENSE updated across the project
    
    * .swiftlint added
    
    * removed retain cycle in location popups
    
    * Project updated to 1.5.0
    
    * Code improved after the review

--- Example/arek_example/ViewController.swift
+++ Example/arek_example/ViewController.swift
@@ -2,8 +2,25 @@
 //  ViewController.swift
 //  arek_example
 //
-//  Created by Ennio Masi on 11/03/2017.
-//  Copyright © 2017 ennioma. All rights reserved.
+//  Copyright (c) 2016 Ennio Masi
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import UIKit
@@ -35,7 +52,7 @@ extension ViewController: UITableViewDataSource {
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ArekCell", for: indexPath) as! ArekCell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArekCell", for: indexPath) as? ArekCell else { fatalError() }
 
         if indexPath.section == 0 {
             cell.viewModel = ArekCellVMServiceProgrammatically.buildVM(index: indexPath.row)
@@ -59,7 +76,7 @@ extension ViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath) as! ArekCell
+        guard let cell = tableView.cellForRow(at: indexPath) as? ArekCell else { fatalError() }
         
         cell.clicked = true
     }
