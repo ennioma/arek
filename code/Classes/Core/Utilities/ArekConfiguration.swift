e6142cb1d40f2fe72c0ea3299671824777008363
Ennio Masi <ennio.masi@gmail.com>
2017-08-26 13:48:30 +0100

    LICENSE updated across the project

--- code/Classes/Core/Utilities/ArekConfiguration.swift
+++ code/Classes/Core/Utilities/ArekConfiguration.swift
@@ -2,8 +2,25 @@
 //  ArekConfiguration.swift
 //  Arek
 //
-//  Created by Ennio Masi on 29/10/2016.
-//  Copyright © 2016 ennioma. All rights reserved.
+//  Copyright (c) 2016 Ennio Masi
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
@@ -46,7 +63,7 @@ public struct ArekConfiguration {
             
             return Calendar.current.compare(lastDate, to: Date(), toGranularity: .hour) == ComparisonResult.orderedDescending
         case .JustOnce:
-            guard let _ = self.lastDateForPermission(identifier: permission.identifier) else {
+            if self.lastDateForPermission(identifier: permission.identifier) == nil {
                 return true
             }
             
@@ -66,6 +83,6 @@ public struct ArekConfiguration {
     }
     
     private func lastDateForPermission(identifier: String) -> Date? {
-        return UserDefaults.standard.object(forKey: identifier) as! Date?
+        return UserDefaults.standard.object(forKey: identifier) as? Date
     }
 }
--- code/Classes/Permissions/ArekNotifications.swift
+++ code/Classes/Permissions/ArekNotifications.swift
@@ -2,8 +2,25 @@
 //  ArekNotifications.swift
 //  arek
 //
-//  Created by Ennio Masi on 08/11/2016.
-//  Copyright © 2016 ennioma. All rights reserved.
+//  Copyright (c) 2016 Ennio Masi
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
@@ -18,7 +35,7 @@ open class ArekNotifications: ArekBasePermission, ArekPermissionProtocol {
         super.init(identifier: self.identifier)
     }
     
-    public override init(configuration: ArekConfiguration? = nil,  initialPopupData: ArekPopupData? = nil, reEnablePopupData: ArekPopupData? = nil) {
+    public override init(configuration: ArekConfiguration? = nil, initialPopupData: ArekPopupData? = nil, reEnablePopupData: ArekPopupData? = nil) {
         super.init(configuration: configuration, initialPopupData: initialPopupData, reEnablePopupData: reEnablePopupData)
     }
 
@@ -48,7 +65,7 @@ open class ArekNotifications: ArekBasePermission, ArekPermissionProtocol {
     open func askForPermission(completion: @escaping ArekPermissionResponse) {
         if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge]) { (granted, error) in
-                if let _ = error {
+                if let error = error {
                     print("[🚨 Arek 🚨] Push notifications permission not determined 🤔, error: \(error)")
                     return completion(.notDetermined)
                 }
@@ -73,4 +90,3 @@ open class ArekNotifications: ArekBasePermission, ArekPermissionProtocol {
         }
     }
 }
-
