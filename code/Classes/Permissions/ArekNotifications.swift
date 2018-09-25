879123aa9aa59c3a0bfd91a5ad7d1db655ea88f9
Yehuda Cohen <cohen72@gmail.com>
2018-09-25 17:51:36 +0300

    Allow compiling on Xcode 10
    After upgrading to Xcode 10, I was unable to compile my project due to a missing switch case for a new case called `provisional`.  Not sure how you want to handle it, but I'd at least like to unblock my project from getting compiled.

--- code/Classes/Permissions/ArekNotifications.swift
+++ code/Classes/Permissions/ArekNotifications.swift
@@ -49,6 +49,8 @@ open class ArekNotifications: ArekBasePermission, ArekPermissionProtocol {
                     return completion(.denied)
                 case .authorized:
                     return completion(.authorized)
+                case .provisional:
+                  break
                 }
             }
         } else if #available(iOS 9.0, *) {
